cdd6c1d1c3c78061b741d13dfb2b9063501c23e3
Gurgen <gsarkisdganyan@gmail.com>
2024-02-12 23:21:33 +0300

    fix: 'убрал статус-коды' '

--- src/domain/comment-service.ts
+++ src/domain/comment-service.ts
@@ -2,30 +2,27 @@ import {CommentRepository} from "../repositories/comment-repository";
 import {CommentInputModel} from "../models/comments/input";
 import {CommentQueryRepository} from "../repositories/comment-query-repository";
 import {UserViewModel} from "../models/users/output";
-import {statusCodeService, StatusCodeType} from "./status-code-service";
 
 export class CommentService {
-    static async deleteComment(id: string, user: Partial<UserViewModel>): Promise<StatusCodeType<null>> {
+    static async deleteComment(id: string, user: Partial<UserViewModel>): Promise<boolean> {
         try {
-            if (!user || !user.id) return statusCodeService("NOT_FOUND", null);
+            if (!user || !user.id) return false;
             const isCommentCanBeDeleted = await CommentRepository.getUserCommentById(id, user.id!)
-            if (!isCommentCanBeDeleted) return statusCodeService("FORBIDDEN", null);
-            const isDeleted =  await CommentRepository.deleteComment(id);
-            return isDeleted ? statusCodeService("NO_CONTENT", null) : statusCodeService("NOT_FOUND", null);
+            if (!isCommentCanBeDeleted) return false;
+            return await CommentRepository.deleteComment(id);
         } catch (e) {
-            return statusCodeService("NOT_FOUND", null);
+            return false
         }
     }
 
-    static async updateComment(id: string, newComment: string, user: Partial<UserViewModel>): Promise<StatusCodeType<null>> {
+    static async updateComment(id: string, newComment: string, user: Partial<UserViewModel>): Promise<boolean> {
         try {
-            if (!user || !user.id) return statusCodeService("NOT_FOUND", null);
+            if (!user || !user.id) return false;
             const isCommentCanBeDeleted = await CommentRepository.getUserCommentById(id, user.id!)
-            if (!isCommentCanBeDeleted) return statusCodeService("FORBIDDEN", null);
-            const isUpdated = await CommentRepository.updateComment(id, newComment);
-            return isUpdated ? statusCodeService("NO_CONTENT", null) : statusCodeService("NOT_FOUND", null);
+            if (!isCommentCanBeDeleted) return false;
+            return await CommentRepository.updateComment(id, newComment);
         } catch (e) {
-            return statusCodeService('NOT_FOUND', null);
+            return false
         }
     }
 
