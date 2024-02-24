67a83921be4da2f4eb2910d0c31448927974e584
Gurgen <gsarkisdganyan@gmail.com>
2024-02-24 21:25:40 +0300

    add (auth): тест просрочки токена

--- src/app/auth/jwt-service.ts
+++ src/app/auth/jwt-service.ts
@@ -6,7 +6,7 @@ export class JwtService {
     static async createJWT(userid: string, expiresIn: string): Promise<string | null> {
         try {
             dotenv.config();
-            return jwt.sign({userId: userid}, process.env.SECRET_WORD!, {expiresIn: '10s'});
+            return jwt.sign({userId: userid}, process.env.SECRET_WORD!, {expiresIn: expiresIn});
         } catch (e) {
             console.error('Error creating JWT:', e);
             return null
