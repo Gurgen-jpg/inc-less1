350c0810bc7107fc70325ddd165898bd4e9209cd
Gurgen <gsarkisdganyan@gmail.com>
2024-03-12 20:18:54 +0300

    deleteAllSessions (fix): добавил исключение

--- src/middlewares/authValidation/refresh-token-validation.ts
+++ src/middlewares/authValidation/refresh-token-validation.ts
@@ -2,12 +2,14 @@ import {NextFunction, Request, Response} from "express";
 import dotenv from "dotenv";
 import { HTTP_STATUSES} from "../../models/common";
 import {JwtService} from "../../app/auth/jwt-service";
+import {AuthRepository} from "../../repositories/auth-repository";
 
-export const refreshTokenMiddleware = (req: Request, res: Response, next: NextFunction) => {
+export const refreshTokenMiddleware = async (req: Request, res: Response, next: NextFunction) => {
     dotenv.config();
     const cookie = req.cookies.refreshToken;
     const isTokenExpired = JwtService.isTokenExpired(cookie);
-    if (isTokenExpired) {
+    const isTokenInBlackList = await AuthRepository.isTokenBlacklisted(cookie);
+    if (isTokenExpired || isTokenInBlackList) {
         return res.sendStatus(HTTP_STATUSES.UNAUTHORIZED);
     }
 
