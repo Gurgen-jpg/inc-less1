be2a416b93ebc25467a95ff82a3f94b893295ada
Gurgen <gsarkisdganyan@gmail.com>
2024-03-15 08:56:18 +0300

    rateLimitMiddleware (feat): add rateLimitMiddleware

--- src/middlewares/rateLimit/rate-limit-middleware.ts
+++ src/middlewares/rateLimit/rate-limit-middleware.ts
@@ -2,7 +2,6 @@ import {NextFunction, Request, Response} from "express";
 import {RateLimitRepository} from "../../repositories/rateLimit-repository";
 
 export const rateLimitMiddleware = async (request: Request, response: Response, next: NextFunction) => {
-    await RateLimitRepository.createRateLimit({ip: request.ip!, url: request.url, date: new Date().toISOString()});
     const count = await RateLimitRepository.count(request.ip!, request.url!);
     if (count === null) {
         return response.sendStatus(429);
@@ -10,5 +9,6 @@ export const rateLimitMiddleware = async (request: Request, response: Response, 
     if (count > 5) {
         return response.sendStatus(429);
     }
+    await RateLimitRepository.createRateLimit({ip: request.ip!, url: request.url, date: new Date().toISOString()});
     return next();
 }
