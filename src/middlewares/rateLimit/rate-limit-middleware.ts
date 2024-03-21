ba6350665382b41a796f81a499bedd22a688e518
Gurgen <gsarkisdganyan@gmail.com>
2024-03-21 19:41:05 +0300

    rateLimitMiddleware (fix): изменил порядок действий

--- src/middlewares/rateLimit/rate-limit-middleware.ts
+++ src/middlewares/rateLimit/rate-limit-middleware.ts
@@ -2,10 +2,11 @@ import {NextFunction, Request, Response} from "express";
 import {RateLimitRepository} from "../../repositories/rateLimit-repository";
 
 export const rateLimitMiddleware = async (request: Request, response: Response, next: NextFunction) => {
-    await RateLimitRepository.createRateLimit({ip: request.ip!, url: request.baseUrl, date: new Date().getTime()});
     const count = await RateLimitRepository.count(request.ip!, request.baseUrl!);
-    if (count !== null && count > 5) {
+    if (count !== null && count >= 5) {
         return response.sendStatus(429);
     }
+    await RateLimitRepository.createRateLimit({ip: request.ip!, url: request.baseUrl, date: new Date().getTime()});
+
     return next();
 }
