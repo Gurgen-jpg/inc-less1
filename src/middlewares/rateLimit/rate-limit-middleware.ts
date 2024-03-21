a3efdc65c742551d583b4b114063569def61cc87
Gurgen <gsarkisdganyan@gmail.com>
2024-03-21 19:43:34 +0300

    rateLimitMiddleware (fix): добавил ответ

--- src/middlewares/rateLimit/rate-limit-middleware.ts
+++ src/middlewares/rateLimit/rate-limit-middleware.ts
@@ -4,7 +4,7 @@ import {RateLimitRepository} from "../../repositories/rateLimit-repository";
 export const rateLimitMiddleware = async (request: Request, response: Response, next: NextFunction) => {
     const count = await RateLimitRepository.count(request.ip!, request.baseUrl!);
     if (count !== null && count >= 5) {
-        return response.sendStatus(429);
+        return response.status(429).send("Too Many Requests");
     }
     await RateLimitRepository.createRateLimit({ip: request.ip!, url: request.baseUrl, date: new Date().getTime()});
 
