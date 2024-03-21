bf639fc69efa656edbfe4e2a7a8e4ad70ca0da99
Gurgen <gsarkisdganyan@gmail.com>
2024-03-21 19:51:06 +0300

    rateLimitMiddleware (fix): изменил url

--- src/middlewares/rateLimit/rate-limit-middleware.ts
+++ src/middlewares/rateLimit/rate-limit-middleware.ts
@@ -2,11 +2,11 @@ import {NextFunction, Request, Response} from "express";
 import {RateLimitRepository} from "../../repositories/rateLimit-repository";
 
 export const rateLimitMiddleware = async (request: Request, response: Response, next: NextFunction) => {
-    const count = await RateLimitRepository.count(request.ip!, request.baseUrl!);
+    const count = await RateLimitRepository.count(request.ip!, request.originalUrl!);
     if (count !== null && count >= 5) {
         return response.status(429).send("Too Many Requests");
     }
-    await RateLimitRepository.createRateLimit({ip: request.ip!, url: request.baseUrl, date: new Date().getTime()});
+    await RateLimitRepository.createRateLimit({ip: request.ip!, url: request.originalUrl, date: new Date().getTime()});
 
     return next();
 }
